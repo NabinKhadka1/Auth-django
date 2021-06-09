8b9a86837d5461370e4c7de944f7661f82836321
nabinkhadka1 <nabinkhadka330@gmail.com>
2021-06-09 13:11:10 +0545

    Initial commit

---
+++ auth_django/settings/env.examples.py
@@ -0,0 +1,17 @@
+from auth_django.settings.env import SECRET_KEY
+
+
+from .base import *
+SECRET_KEY = 'Your Secret Key here'
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
